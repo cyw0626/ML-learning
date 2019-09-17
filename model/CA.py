0ebb31b95d9091577a4b5b3a7a25c7a15a896d7e
Yuwang Cui <44495079+cyw0626@users.noreply.github.com>
2019-09-17 12:59:43 +0800

    Create CA.py

---
+++ model/CA.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 空间大小
+number = 200
+
+# 细胞分布(在初始位置状态设为1)
+cells = np.zeros((number, number))
+cells[int(number/2)][int(number/2)-1] = 1
+cells[int(number/2)][int(number/2)] = 1
+cells[int(number/2)][int(number/2)+1] = 1
+
+
+# 绘图(初始情况的图)
+def paintCells(cells):
+    x = []
+    y = []
+    length = len(cells)
+    for i in range(length):
+        for j in range(length):
+            if cells[i][j] == 1:
+                x.append(i)
+                y.append(j)
+    plt.xlim((0, length))
+    plt.ylim((0, length))
+    plt.plot(x, y, '.')
+    plt.show()
+    plt.cla()
+
+
+# 统计，在范围内有多少个状态为1的细胞
+def countCells(cells, i, j):
+    count = 0
+    for m in range(i-1, i+2):
+        for n in range(j-1, j+2):
+            if cells[m][n] == 1:
+                count += 1
+    return count
+
+
+# 更新
+def updateCells(cells):
+    length = len(cells)
+    ncells = np.zeros((length, length))
+    for i in range(1, length-1):
+        for j in range(1, length-1):
+            count = countCells(cells, i, j)
+            if count == 2 or count == 3:
+                ncells[i][j] = 1
+    return ncells
+
+
+paintCells(cells)
+for _ in range(80):
+    cells = updateCells(cells)
+paintCells(cells)
